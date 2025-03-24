7968d49a9ba9e1411de55f2da3738e13c17ee440
4hey4hey <4hey4hey@gmail.com>
2025-03-24 16:52:26 +0900

    修正

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,17 +2,6 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-// Firebase の設定
-// 重要：正確な値を使用してください
-const firebaseConfig = {
-  apiKey: "AIzaSyCi9zffZ9DKqlH20MlYlMOdgIcgyWYsqdk",
-  authDomain: "history-9a233.firebaseapp.com",
-  projectId: "history-9a233",
-  storageBucket: "history-9a233.appspot.com",
-  messagingSenderId: "628189169902",
-  appId: "1:628189169902:web:fc8d5279045f1771498bff",
-  measurementId: "G-D1T96ND9ZE"
-};
 
 // コレクションID
 export const COLLECTION_ID = 'learning-app';
